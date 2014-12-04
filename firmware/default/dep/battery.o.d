db15f5cacf8783a4b731ed9e63b9deb2672efe71
Michael Shimniok <shimniok@gmail.com>
2014-12-03 22:00:33 -0700

    Updated build files

--- firmware/default/dep/battery.o.d
+++ firmware/default/dep/battery.o.d
@@ -4,7 +4,7 @@ battery.o: ../battery.c ../config.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/inttypes.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/include/stdint.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h \
- c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13a.h \
+ c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h \
@@ -29,7 +29,7 @@ c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.
 
 c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h:
 
-c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13a.h:
+c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13.h:
 
 c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h:
 
--- firmware/default/dep/buzzer.o.d
+++ firmware/default/dep/buzzer.o.d
@@ -4,7 +4,7 @@ buzzer.o: ../buzzer.c ../config.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/inttypes.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/include/stdint.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h \
- c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13a.h \
+ c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h \
  c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h \
@@ -24,7 +24,7 @@ c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.
 
 c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h:
 
-c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13a.h:
+c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iotn13.h:
 
 c:\program\ files\ (x86)\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h:
 
