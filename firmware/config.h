7a82f9b1a08c937d68095785f8c8a6054ceb570c
Michael Shimniok <shimniok@gmail.com>
2014-12-03 21:48:17 -0700

    Removed VOLT_LOW which is now unused

--- firmware/config.h
+++ firmware/config.h
@@ -7,7 +7,6 @@
 #define BUZZ		0		// PB0
 
 #define PERIOD		10		// Delay period between signal (seconds) -- initial period
-#define VOLT_LOW    35		// considered low battery voltage; need to conserve more energy; ADC: 255/1.1V; 3.5V->214
 #define PERIOD_LOW  60		// If battery voltage is very low, use long period
 #define WARN_TIME   5*60	// Delay before warning starts sounding (seconds) every PERIOD seconds
 #define SOS_TIME	10*60	// Delay before warning stops and SOS starts sounding (seconds) every PERIOD seconds
