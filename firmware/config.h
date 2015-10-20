640f9a4039946378d210c109be209bd61d4ed19a
shimniok@gmail.com <shimniok@gmail.com>
2015-10-20 14:35:22 -0600

    redefined pins as PB? instead of just 0, 1, 2, ...

--- firmware/config.h
+++ firmware/config.h
@@ -5,8 +5,8 @@
 
 #define F_CPU		125000 // 8MHz / 64
 
-#define BUZZ		0		// PB0
-#define SWITCH  2   // PB2
+#define BUZZ		PB0		// PB0
+#define SWITCH  PB2   // PB2
 
 #define PERIOD		  10		// Delay period between signal (seconds) -- initial period
 #define PERIOD_LOW  60		// If battery voltage is very low, use long period
