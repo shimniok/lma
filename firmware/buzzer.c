b22c77063c148b098abbafd41d665b403d716cfc
shimniok@gmail.com <shimniok@gmail.com>
2015-10-20 10:17:22 -0600

    Modified buzzer so output is set low when not beeping and clock is turned off

--- firmware/buzzer.c
+++ firmware/buzzer.c
@@ -2,28 +2,30 @@
 #include <avr/io.h>
 #include "buzzer.h"
 
-void initBuzzer()
+inline void initBuzzer()
 {
     // Need 3.8kHz tone, 150kHz/2/3800
-	// set freq with output compare register
-    // Want to toggle OC0A on Compare Match: COM0A1=0 (TCCR0A:7), COM0A0=1 (TCCR0A:6) for non-PWM (CTC) mode
-    // CTC mode -- clear on timer compare: WGM02=0 WGM01=1 WGM00=0
+    // set freq with output compare register
     // Prescale /1: CS02=0 CS01=0 CS00=1
     TCNT0=0;
     OCR0A  = F_CPU/2/3800;
-    TCCR0A = _BV(COM0A0) | _BV(WGM01);
 
-	// Set LED and Buzzer as output
-	DDRB |= _BV(BUZZ);
+    // Set LED and Buzzer as output
+    DDRB |= _BV(BUZZ);
 }
 
 
-void beepOn()
+inline void beepOn()
 {
-	TCCR0B |= _BV(CS00);
+    // Want to toggle OC0A on Compare Match: COM0A1=0 (TCCR0A:7), COM0A0=1 (TCCR0A:6) for non-PWM (CTC) mode
+    // CTC mode -- clear on timer compare: WGM02=0 WGM01=1 WGM00=0
+    TCCR0A = _BV(COM0A0) | _BV(WGM01); 
+    TCCR0B = _BV(CS00); // no clock prescaling
 }
 
-void beepOff()
+inline void beepOff()
 {
-	TCCR0B &= ~_BV(CS00);
+    // disable timer, clock
+    TCCR0A = 0;
+    TCCR0B = 0;
 }
