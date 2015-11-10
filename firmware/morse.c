664e29ed60a2e2d0f543a00338deee4a77f4fb4f
shimniok@gmail.com <shimniok@gmail.com>
2015-11-10 15:42:38 -0700

    add numeric beep function

--- firmware/morse.c
+++ firmware/morse.c
@@ -28,6 +28,18 @@ void w()
 	dit(); dah(); dah(); space();
 }
 
+void number(uint8_t n) 
+{
+	// Beep out the current warning time
+	while (n >= 10) {
+		dah();
+		n -= 10;
+	}
+	if (n > 0) {
+		dit();
+	}
+}
+
 void space()
 {
 	_delay_ms(DOT*2); // because we've already delayed 1x
