3c4e6b641bd1a071cd5bde3baff62b8ed06f885d
Michael Shimniok <shimniok@gmail.com>
2021-10-25 16:14:31 -0600

    simplify morse code, parse string of . and -

--- firmware/morse.c
+++ firmware/morse.c
@@ -1,36 +1,46 @@
 #include "config.h"
+#include "clock.h"
 #include <util/delay.h>
 #include "morse.h"
 #include "buzzer.h"
 
-void sos()
+
+void dit()
 {
-	s(); o(); s(); space();
+	beepOn();
+	wait_ms(DOT);
+	beepOff();
+	wait_ms(DOT); // intra-character space
 }
 
-void k()
+void dah()
 {
-	dah(); dit(); dah(); space();
+	beepOn();
+	wait_ms(DASH);
+	beepOff();
+	wait_ms(DOT); // intra-character space
 }
 
-void o()
-{
-	dah(); dah(); dah(); space();
+void message(char *m) {
+	char *s;
+	for (s = m; *s != 0; s++) {
+		switch (*s) {
+		case '.':
+			dit();
+			break;
+		case '-':
+			dah();
+			break;
+		case ' ':
+			space();
+			break;
+		}
+	}
 }
 
-void s()
+// not actually morse numbers..
+void number(uint8_t n)
 {
-	dit(); dit(); dit(); space();
-}
-
-void w()
-{
-	dit(); dah(); dah(); space();
-}
-
-void number(uint8_t n) 
-{
-	// Beep out the current warning time
 	while (n >= 10) {
 		dah();
 		n -= 10;
@@ -38,25 +48,4 @@ void number(uint8_t n)
 	if (n > 0) {
 		dit();
 	}
-}
-
-void space()
-{
-	_delay_ms(DOT*2); // because we've already delayed 1x
-}
-
-void dit()
-{
-	beepOn();
-	_delay_ms(DOT);
-	beepOff();
-	_delay_ms(DOT);
-}
-
-void dah()
-{
-	beepOn();
-	_delay_ms(DOT*3);
-	beepOff();
-	_delay_ms(DOT);
 }
--- firmware/morse.h
+++ firmware/morse.h
@@ -1,18 +1,22 @@
 #ifndef __MORSE_H
 #define __MORSE_H
 
-#define DOT 80						// length of dot in ms
+// times in ms
+#define DOT       60
+#define DASH      DOT*3
+#define SPACE     DOT    // intra character space
+#define WORDSPACE DOT*7  // intra word space
 
-#define ok() o(); k();
+#define space() _delay_ms(DOT*2)
 
-void sos();
-void k();
-void o();
-void s();
-void w();
-void number(uint8_t n);
+#define OK "--. -.-"
+#define SOS "... --- ..."
+#define S "..."
+#define W ".--"
+
 void dit();
 void dah();
-void space();
+void message(char *m);
+void number(uint8_t n);
 
 #endif
