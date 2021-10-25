74beadb883bf83f1e27a7cb167be20e9eeccf49f
Michael Shimniok <shimniok@gmail.com>
2021-10-25 16:13:59 -0600

    add wait_ms to account for prescaler

--- firmware/clock.c
+++ firmware/clock.c
@@ -1,8 +1,17 @@
 #include <avr/interrupt.h>
+#include <util/delay.h>
+#include "clock.h"
 
-// 8MHz / 64 = 125000
+volatile uint16_t millis = 0;
+
 void slowClock()
 {
-	CLKPR = _BV(CLKPCE); // enable change to clock prescaler
-	CLKPR = _BV(CLKPS2) | _BV(CLKPS1); // scale = /64
+	//CLKPR = _BV(CLKPCE); // enable change to clock prescaler
+	//CLKPR = _BV(CLKPS2); // scale = /16
+}
+
+void wait_ms(uint16_t ms)
+{
+  for (int i=0; i < ms; i++)
+    _delay_ms(1);
 }
--- firmware/clock.h
+++ firmware/clock.h
@@ -1,6 +1,11 @@
 #ifndef __clock_h
 #define __clock_h
 
+#define F_PRESCALER 1
+
+#include <avr/io.h>
+
+void wait_ms(uint16_t ms);
 void slowClock();
 
 #endif
