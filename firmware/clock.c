dc564e2decffabe3e03824deb2cbc5ae260866d6
Michael Shimniok <shimniok@gmail.com>
2021-10-26 09:11:19 -0600

    define prescalers in .h, function to return prescale value

--- firmware/clock.c
+++ firmware/clock.c
@@ -4,10 +4,36 @@
 
 volatile uint16_t millis = 0;
 
+uint8_t get_cpu_prescaler()
+{
+	switch (F_PRESCALER) {
+		case F_PRESCALER_1:
+			return 1;
+		case F_PRESCALER_2:
+			return 2;
+		case F_PRESCALER_4:
+		  return 4;
+		case F_PRESCALER_8:
+		  return 8;
+		case F_PRESCALER_16:
+			return 16;
+		case F_PRESCALER_32:
+		  return 32;
+		case F_PRESCALER_64:
+		  return 64;
+		case F_PRESCALER_128:
+		  return 128;
+		case F_PRESCALER_256:
+		  return 256;
+		default:
+			return 1;
+	}
+}
+
 void slowClock()
 {
-	//CLKPR = _BV(CLKPCE); // enable change to clock prescaler
-	//CLKPR = _BV(CLKPS2); // scale = /16
+	CLKPR = _BV(CLKPCE); // enable change to clock prescaler
+	CLKPR = F_PRESCALER;
 }
 
 void wait_ms(uint16_t ms)
--- firmware/clock.h
+++ firmware/clock.h
@@ -1,9 +1,19 @@
 #ifndef __clock_h
 #define __clock_h
 
-#define F_PRESCALER 1
-
 #include <avr/io.h>
+
+#define F_PRESCALER_1    0
+#define F_PRESCALER_2    _BV(CLKPS0)
+#define F_PRESCALER_4    _BV(CLKPS1)
+#define F_PRESCALER_8    (_BV(CLKPS1)|_BV(CLKPS0))
+#define F_PRESCALER_16   _BV(CLKPS2)
+#define F_PRESCALER_32   (_BV(CLKPS2)|_BV(CLKPS0))
+#define F_PRESCALER_64   (_BV(CLKPS2)|_BV(CLKPS1))
+#define F_PRESCALER_128  (_BV(CLKPS2)|_BV(CLKPS1)|_BV(CLKPS0))
+#define F_PRESCALER_256  _BV(CLKPS3)
+
+#define F_PRESCALER F_PRESCALER_1
 
 void wait_ms(uint16_t ms);
 void slowClock();
